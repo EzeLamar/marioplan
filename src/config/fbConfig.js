2ebda8f561c38bc245143e287820ea1f3b1ff8a2
Ezequiel Lamarque <ezequiel.lamarque@globant.com>
2019-11-22 10:55:40 -0300

    added config firebase
    fixed problem with firebase

---
+++ src/config/fbConfig.js
@@ -0,0 +1,21 @@
+
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Your web app's Firebase configuration
+var config = {
+    apiKey: "AIzaSyDDYp3ED8Br8TO7xWd_ROErdhVN2SCJQEg",
+    authDomain: "net-ninja-marioplan-41abf.firebaseapp.com",
+    databaseURL: "https://net-ninja-marioplan-41abf.firebaseio.com",
+    projectId: "net-ninja-marioplan-41abf",
+    storageBucket: "net-ninja-marioplan-41abf.appspot.com",
+    messagingSenderId: "909936247087",
+    appId: "1:909936247087:web:ceeeb22ae43d1df492e1da",
+    measurementId: "G-E4XQW7H4QS"
+  };
+// Initialize Firebase
+firebase.initializeApp(config);
+//firebase.firestore().settings({ timestampsInShots:true })
+
+export default firebase;

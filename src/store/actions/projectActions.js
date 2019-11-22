ed578cc5100608ed514ab6610f07562d1a05741e
Ezequiel Lamarque <ezequiel.lamarque@globant.com>
2019-11-22 13:13:27 -0300

    added push request of new project to firebase

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,18 @@
+import firebase from '../../config/fbConfig'
+
 export const createProject = (project) => {
     return (dispatch, getState) => {
         // make async call to database
-        dispatch({type: 'CREATE_PROJECT', project});
+        const firestore = firebase.firestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'eze',
+            authorLastName: 'lamar',
+            authorId: 12345,
+            createAt: new Date(),
+        }).then(() => { dispatch({type: 'CREATE_PROJECT', project});
+        }).catch((err) => {
+            dispatch({ type: 'CREATE_PROJECT_ERROR', err });
+        })
     }
 }
